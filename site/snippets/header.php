1152cad748dfcc709696c0315373ba5ea56ac51a
Gregor Holtz <debrisplease@gmail.com>
2015-04-28 21:28:33 -0700

    add max width to outer-container and fixed titles

--- site/snippets/header.php
+++ site/snippets/header.php
@@ -5,7 +5,11 @@
   <meta charset="utf-8" />
   <meta name="viewport" content="width=device-width,initial-scale=1.0">
 
-  <title><?php echo $site->title()->html() ?> | <?php echo $page->title()->html() ?></title>
+  <title><?php echo $site->title()->html() ?>
+    <?php if(!$page->isHomepage()) {
+      echo '| ';
+      echo $page->title()->html(); } ?>
+      </title>
   <meta name="description" content="<?php echo $site->description()->html() ?>">
   <meta name="keywords" content="<?php echo $site->keywords()->html() ?>">
 
