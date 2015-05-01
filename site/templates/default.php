679ede09b7ce0ae88ef7c331560e6394d3a5b8d0
Gregor Holtz <debrisplease@gmail.com>
2015-04-30 20:09:40 -0700

    add pages styles and inner-container

--- site/templates/default.php
+++ site/templates/default.php
@@ -1,12 +1,15 @@
 <?php snippet('header') ?>
+  <div class="inner-container">
 
-  <main class="main" role="main">
+    <main class="main" role="main">
 
-    <div class="text">
-      <h1><?php echo $page->title()->html() ?></h1>
-      <?php echo $page->text()->kirbytext() ?>
-    </div>
+      <div class="text">
+        <h1><?php echo $page->title()->html() ?></h1>
+        <?php echo $page->text()->kirbytext() ?>
+      </div>
 
-  </main>
+    </main>
+
+  </div>
 
 <?php snippet('footer') ?>
--- site/templates/home.php
+++ site/templates/home.php
@@ -4,21 +4,27 @@
   <source src="<?php echo url('assets/images/gholtz.webm') ?>" type="video/webm">
   <source src="<?php echo url('assets/images/gholtz.mp4') ?>" type="video/mp4">
   </video>
+  <div class="inner-container">
 
-  <main class="main" role="main">
+    <main class="main" role="main">
 
-    <div class="text">
-      <!-- <h1><?php echo $page->title()->html() ?></h1> -->
-      <?php echo $page->text()->kirbytext() ?>
-    </div>
-
-    <a href="#">
-      <div class="interested">
-        <h2>Interested?</h2>
+      <div class="text">
+        <!-- <h1><?php echo $page->title()->html() ?></h1> -->
+        <?php echo $page->text()->kirbytext() ?>
       </div>
-    </a>
 
+      <a href="#">
+        <div class="interested">
+          <h2>Interested?</h2>
+        </div>
+      </a>
 
-  </main>
+      <div class="philosophy">
+
+      </div>
+
+    </main>
+
+  </div>
 
 <?php snippet('footer') ?>
